b6985de25a61951e624cb4c5a10ad96e00447c05
sue4869 <sue4869@naver.com>
2024-11-03 00:35:05 +0900

    init

---
+++ src/test/java/sumin/com/paypay/PayPayApplicationTests.java
@@ -0,0 +1,13 @@
+package sumin.com.paypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
